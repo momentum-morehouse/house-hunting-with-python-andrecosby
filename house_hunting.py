6308b62304e9aed8392c004fcc8de6c9fa9297cb
Andre Cosby <andrecosby1@gmail.com>
2020-09-29 10:10:18 -0400

    hmwrk

--- house_hunting.py
+++ house_hunting.py
@@ -3,14 +3,17 @@ annual_salary = input("Enter your annual salary: ")
 portion_saved = input("enter your percent of your salary to save as a decimal: ")
 monthly_salary_saved = input(int(annual_salary)/12 * float(portion_saved))
 total_cost = input("Enter cost of home: ")
-porion_down_payment = .25 * int(total_cost)
+print(total_cost)
+portion_down_payment = .25 * int(total_cost)
+print(portion_down_payment)
 
-current_savings = 0
+
+# current_savings = 0
 
 i = 1
-while current_savings < porion_down_payment:
+while current_savings < portion_down_payment:
     current_savings += monthly_salary_saved
     investment_return = current_savings * (.04/12)
     current_savings += investment_return
     1 += 1
-print("it will take you",i,"months to save for downpayment!" )
+print("Number",i,"months to save for downpayment for house" )
